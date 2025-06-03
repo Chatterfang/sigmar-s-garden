9043163ea574b80a375429717cde7ebc9f0a1418
David Mancel <xmanced00@stud.fit.vutbr.cz>
2025-06-03 23:05:54 +0200

    added TODO tasks

--- board_layout.py
+++ board_layout.py
@@ -2,3 +2,13 @@ import random
 import tkinter as tk
 
 #basic board layout generator
+
+#basic tasks
+#TODO: figure out how to make a hex board
+#TODO: figure out how to check hex surroundings
+#gui tasks
+#TODO: basic board layout
+#TODO: interactive buttons/objects
+#level generator tasks
+#TODO: fully random level gen
+#TODO: figure out logic to minimize impossible levels
